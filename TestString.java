edfbb10c8dd913e0004ec19efb1fcbcf0cd5a47c
Seoryeon <79080533+sryeon1021@users.noreply.github.com>
2021-04-01 17:39:45 +0900

    Add files via upload

---
+++ TestString.java
@@ -0,0 +1,9 @@
+public class TestString {
+    public static void main(String[] args) {
+        String a = "Hello Java";
+        System.out.println(a.indexOf("Java"));
+        System.out.println(a.replaceAll("Java", "World"));
+        System.out.println(a.substring(0, 4));
+        System.out.println(a.toUpperCase());
+    }
+}
